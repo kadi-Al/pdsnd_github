6b286b999fd49069d92d0e5994b9abaca5ab4c79
kadi-Al <KadyAlsaif@outlook.com>
2025-09-29 16:32:36 +0300

    refactor: Improve output message in bikeshare_2.py

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -207,7 +207,7 @@ def main():
         user_stats(df)
         display_data(df)
 
-        restart = input('\Do you like to restart? Enter yes or no.\n')
+        restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
 
